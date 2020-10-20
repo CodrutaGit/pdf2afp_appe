bf39c3630296df0b417a9a87095f7ad5a70e40e0
TransformsGit <64213593+TransformsGit@users.noreply.github.com>
2020-10-20 13:16:32 +0300

    Add files via upload

--- 4create_folder_structure_PDF2AFP_APPE_win.bat
+++ 4create_folder_structure_PDF2AFP_APPE_win.bat
@@ -15,12 +15,12 @@ echo mkdir E:\OutputTransformPDF2AFP_APPE\win\output\out_AFP\TestFiles
 mkdir E:\OutputTransformPDF2AFP_APPE\win\output\out_PDF\TestFiles
 echo mkdir E:\OutputTransformPDF2AFP_APPE\win\output\out_PDF\TestFiles
 
-mkdir E:\OutputTransformPDF2AFP_APPE\win\output\out_TIFF\MF
-echo mkdir E:\OutputTransformPDF2AFP_APPE\win\output\out_TIFF\MF
+mkdir E:\OutputTransformPDF2AFP_APPE\win\output\out_TIFF\MasterFiles
+echo mkdir E:\OutputTransformPDF2AFP_APPE\win\output\out_TIFF\MasterFiles
 
 c:\Windows\System32\xcopy.exe /T /e e:\TestFiles_folder_structure\TestFiles\* E:\OutputTransformPDF2AFP_APPE\win\output\out_AFP\TestFiles\ /s /i
 c:\Windows\System32\xcopy.exe /T /e e:\TestFiles_folder_structure\TestFiles\* E:\OutputTransformPDF2AFP_APPE\win\output\out_PDF\TestFiles\ /s /i
-c:\Windows\System32\xcopy.exe /T /e e:\TestFiles_folder_structure\TestFiles\* E:\OutputTransformPDF2AFP_APPE\win\output\out_TIFF\MF\ /s /i
+c:\Windows\System32\xcopy.exe /T /e e:\TestFiles_folder_structure\TestFiles\* E:\OutputTransformPDF2AFP_APPE\win\output\out_TIFF\MasterFiles\ /s /i
 
 
 echo Folder structure succesfully done!
